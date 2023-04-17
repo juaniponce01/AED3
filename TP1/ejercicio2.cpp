d5a68c9deda77c4061274546b578faf73bf3bf89
juaniponce01 <62446939+juaniponce01@users.noreply.github.com>
2023-04-17 13:38:54 -0300

    ej2 no corre los tests del juez

--- TP1/ejercicio2.cpp
+++ TP1/ejercicio2.cpp
@@ -5,67 +5,111 @@
 #include <ctime>
 
 using namespace std;
+long long N, divisor, resto; // parametros de entrada
+//bool suma, mult, expo, res; // suma, multiplicacion, exponenciacion, resta
+bool corta = false;
+vector<vector<int>> DP; // resto x N
+vector<long long> v; // vector de numeros
 
-int N, divisor, resto; // parametros de entrada
-int suma, mult, expo, res; // suma, multiplicacion, exponenciacion, resta
-vector<int> v; // vector de numeros
+long long mod(long long a, long long b){
+    return (b + (a % b)) % b;
+}
 
-
-bool operadores(int i, int t){
-    if (i == N){
-        if (t % divisor == resto){
-            return true;
-        } else {
-            return false;
-        }
+long long a;
+long long expBS(long long b, long long e){
+    if (e == 0){
+        return 1;
     } else {
-        suma = operadores(i+1, (t+v[i]) % divisor);
-        mult = operadores(i+1, (t*v[i]) % divisor);
-//        expo = operadores(i+1, pow(t, v[i]) % divisor);
-        res = operadores(i+1, t-v[i]);
-
-        return suma or mult or expo or res;
+        if (e % 2 == 0){
+            a = expBS(b,e/2) % divisor;
+            return (a * a) % divisor;
+        } else {
+            a = expBS(b,(e-1)/2) % divisor;
+            return (b * a * a) % divisor;
+        }
     }
 }
 
-vector<string> split(string str, string separador){
-    vector<string>result;
-    while(str.size()){
-        int index = str.find(separador);
-        if(index!=string::npos){
-            result.push_back(str.substr(0,index));
-            str = str.substr(index+separador.size());
-            if(str.size()==0)result.push_back(str);
-        }else{
-            result.push_back(str);
-            str = "";
-        }
+//bool operadores(long long i, long long t){
+//    bool suma, mult, expo, res; // suma, multiplicacion, exponenciacion, resta
+//    if (DP[i][t] != -1) {
+//        if (DP[i][t]==1){
+//            corta = true;
+//        }
+//    } else {
+//        if (corta){
+//            DP[i][t] = 1;
+//            return true;
+//        }
+//
+//        suma = operadores(i+1, (t % divisor + v[i] % divisor) % divisor);
+//        mult = operadores(i+1, (t % divisor * v[i] % divisor) % divisor);
+//        expo = operadores(i+1, expBS(t % divisor, v[i] % divisor));
+//        res = operadores(i+1, mod((t % divisor) - (v[i] % divisor), divisor));
+//
+//        DP[i][t] = (suma || mult || expo || res);
+//    }
+//    return DP[i][t];
+//}
+
+bool operadoresBU(long long i, long long t){  // bottom up
+    bool suma, mult, expo, res; // suma, multiplicacion, exponenciacion, resta
+    for (int j = i-1; j >= 0; j--){
+        suma = DP[j+1][mod((t % divisor) - (v[j] % divisor), divisor)];
+        mult = DP[j+1][(t % divisor) / (v[j] % divisor) % divisor];
+        expo = DP[j+1][expBS(t % divisor, v[j] % divisor)];
+        res = DP[j+1][(t % divisor + v[j] % divisor) % divisor];
+        DP[j][t] = (suma || mult || expo || res);
     }
-    return result;
+    return DP[0][0];
+}
+
+bool operadores2(long long i, long long t){
+    if (DP[i][t] == -1) {
+        DP[i][t] = (operadores2(i+1, (t % divisor + v[i] % divisor) % divisor) ||
+                    operadores2(i+1, (t % divisor * v[i] % divisor) % divisor) ||
+                    operadores2(i+1, expBS(t % divisor, v[i] % divisor)) ||
+                    operadores2(i+1, mod((t % divisor) - (v[i] % divisor), divisor)));
+    }
+    return DP[i][t];
 }
 
 int main(){
     cout << "Ejercicio Programacion Dinamica: Operadores" << endl;
     int casos;
     cin >> casos;
+    vector<bool> existe(casos, false);
+    vector<double> tiempos(casos, 0);
     for (int i = 0; i < casos; i++){
         cin >> N >> resto >> divisor;
-
+        unsigned t0, t1;
+        v.resize(N);
+        corta = false;
 
         for (int j = 0; j < N; j++){
             cin >> v[j];
         }
 
-        unsigned t0, t1;
+        DP.clear();
+        DP.resize(N+1, vector<int>(divisor, -1));
+
+        for (int j = 0; j < divisor; j++) {
+            DP[N][j] = resto == j;
+        }
 
         t0 = clock();
-        operadores(0, 0);
+        existe[i] = operadores2(0, 0);
         t1 = clock();
 
         double time = (double(t1-t0)/CLOCKS_PER_SEC);
-        cout << "Execution Time: " << time << " seg" << endl;
+        tiempos[i] = time;
     }
-    cout << casos << endl;
-    cout << numeros << endl;
+    for (bool caso:existe){
+        if (caso){
+            cout<<"Si"<<endl;
+        }else{
+            cout<<"No"<<endl;
+        }
+    }
     return 0;
 }
