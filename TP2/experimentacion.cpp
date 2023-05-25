9299c33c507d7c9b0d213502fb500092af239dd8
juaniponce01 <juaniponce0@gmail.com>
2023-05-24 23:06:36 -0300

    experimentacion funcionando

---
+++ TP2/experimentacion.cpp
@@ -0,0 +1,196 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unsigned t1, t2;
+int C, n, R, W, U ,V;
+int global_cod;
+double res_UTP, res_FO;
+vector<pair<double, double>> result;
+vector<double> tiempos;
+/*
+ * c es Cantidad de Casos
+ * N es la cantidad de oficinas
+ * R es el radio maximo en el que puedo extender un cable UTP
+ * W es la cantidad de modems
+ * U es el precio de UTP
+ * V es el precio del cable de fibra optica
+ */
+vector<vector<pair<int, int>>> aristas;
+vector<pair<int, int>> oficinas;
+vector<tuple<double, int, int>> E;
+vector<tuple<double, int, int>> agm;
+
+struct DSU{
+    void resize(int n){
+        padre.resize(n+1);
+        rank.resize(n+1);
+        for(int v = 1; v <= n; v++) padre[v] = v;
+    }
+
+    int findPC(int v){
+        if(v == padre[v]) return v;
+        return padre[v] = find(padre[v]);
+    }
+
+    int find(int v){
+        if(v == padre[v]) return v;
+        return find(padre[v]);
+    }
+
+    void uniteUR(int u, int v){
+        u = find(u), v = find(v);
+        if(u == v) return;
+        if(rank[u] < rank[v]) swap(u,v);
+        padre[v] = padre[u];
+        rank[u] = max(rank[u],rank[v]+1);
+    }
+
+    void unite(int u, int v){
+        u = find(u), v = find(v);
+        if(u == v) return;
+        //if(rank[u] < rank[v]) swap(u,v);
+        padre[v] = padre[u];
+        //rank[u] = max(rank[u],rank[v]+1);
+    }
+
+    void clear(){
+        padre.clear();
+        rank.clear();
+    }
+
+    vector<int> padre;
+    vector<int> rank;
+};
+
+DSU dsu;
+
+//================= IDEA ================================
+/*
+ * 1) generar el grafo completo: tener e cuenta que cada arista tiene un peso que varia dependiendo de la distancia R
+ * si la distancia es < R entonces usamos U, caso contrario usamos solo V
+ * 2) hacer el agm con kruskal
+ * 3) sacar las w aristas de mayor peso
+ * 4) calcular el peso total de las w aristas
+ */
+
+double distancia(pair<int, int> a, pair<int, int> b){
+    return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
+}
+
+bool puedoCable(pair<int, int> a, pair<int, int> b){
+    return distancia(a, b) <= R;
+}
+
+void completarGrafo(){
+    for (int i = 1; i <= n; i++){
+        for (int j = i+1; j <= n; j++){
+            double peso;
+            double dist = distancia(oficinas[i], oficinas[j]);
+            if (dist <= (double)R){ // usar UTP
+                peso = (double)U * dist;
+            } else { // usar fibra optica
+                peso = (double)V * dist;
+            }
+            E.emplace_back(peso, i, j);
+        }
+    }
+}
+
+void kruskal(){
+    int cant_aristas = 0;
+    dsu.clear();
+    dsu.resize(n);
+    for(auto arista : E){
+        double p = get<0>(arista); // Peso
+        int v = get<1>(arista); // Vertice 1
+        int u = get<2>(arista); // Vertice 2
+        //si (u,v) es arista segura
+
+
+        //case 0: // uniteUR y findPC
+        //case 1: // uniteUR y find
+        //case 2: // unite y findPC
+        //case 3: // unite y find
+        bool guarda;
+        if (global_cod == 0 or global_cod == 2){
+            guarda = dsu.findPC(v) != dsu.findPC(u);
+        } else{
+            guarda = dsu.find(v) != dsu.find(u);
+        }
+
+        if(guarda){
+            // agregar
+            if(global_cod <=1){
+                dsu.uniteUR(u, v);
+            }
+            else{
+                dsu.unite(u, v);
+            }
+
+            //dsu.unite(u,v);
+            cant_aristas++;
+            agm.push_back(arista);
+            if (puedoCable(oficinas[v], oficinas[u])) {
+                res_UTP += p;
+            } else {
+                res_FO += p;
+            }
+        }
+        if (cant_aristas == n-W) break;
+    }
+}
+
+void guardarTiempos(ostream file){
+    // n | t0 | t1 | t2 | t3
+    file << n << " ";
+    for(auto & t : tiempos){
+        file << t << " ";
+    }
+    file << endl;
+}
+
+int main(){
+    ofstream file;
+    file.open("exp2.csv");
+    file << "n " << "opt " << "ur " << "pc " << "peor " << endl;
+    R = 5, U = 5, V = 8;
+    n = 2;
+    while (n <= 1000){
+        file << n << " ";
+        int W = n / 2;
+        tiempos.clear();
+        res_UTP = 0, res_FO = 0;
+        E.clear();
+        agm.clear();
+        oficinas.resize(n+1);
+
+        srand(time(NULL));
+        for (int j = 1; j <= n; j++){
+            int x, y;
+            //cin >> x >> y;
+            x = (rand() % (n*10+1)) + 1;
+            y = (rand() % (n*10+1)) + 1;
+            oficinas[j] = make_pair(x, y);
+        }
+
+        completarGrafo();
+        sort(E.begin(),E.end());
+
+        for (int cod = 0; cod < 4; cod++){
+            global_cod = cod;
+
+            t1 = clock();
+            kruskal();
+            t2 = clock();
+
+            file << ((double)(t2 - t1)/(double)CLOCKS_PER_SEC) << " ";
+        }
+        file << endl;
+        cout << ((double)n/1000)*100 << "% ... " << endl;
+        n += 10;
+    }
+    file.close();
+
+    return 0;
+}
