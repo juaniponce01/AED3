8a722b7fd18db70bfcb7e9b14d12dc4b9d7ec379
juaniponce01 <juaniponce0@gmail.com>
2023-06-18 15:11:34 -0300

    ej2 resuelto y pasa el juez

--- TP3/ejercicio2.cpp
+++ TP3/ejercicio2.cpp
@@ -1,24 +1,24 @@
 #include <vector>
 #include <tuple>
-#include <iostream>
 #include <climits>
 #include <queue>
 #include <algorithm>
+
 using namespace std;
 
-vector<vector<tuple<int, int>>> aristas; // aristas[i] = vector de tuplas (j, capacidad)
+// 0 0 para terminar
+// m lineas con dos numeros i y j, indicando que los estudiantes i y j son amigos
+// una linea con n numeros, donde el i-esimo numero es el algoritmo favorito del estudiante i, siendo 1 para Prim y 0 para Kruskal
+// n, m cantidad de estudiantes y aristas
+//votar a favor de su algoritmo favorito pero genera tension = disconformidad
+//votar en contra de su algoritmo favorito = disconformidad
+
+//https://cp-algorithms.com/graph/edmonds_karp.html#implementation
+
 vector<int> estudiantes;
 
 int n, m;
 
-//votar en contra de su algoritmo favorito = disconformidad
-//votar a favor de su algoritmo favorito pero genera tension = disconformidad
-// n, m cantidad de estudiantes y aristas
-// una linea con n numeros, donde el i-esimo numero es el algoritmo favorito del estudiante i, siendo 1 para Prim y 0 para Kruskal
-// m lineas con dos numeros i y j, indicando que los estudiantes i y j son amigos
-// 0 0 para terminar
-
-//https://cp-algorithms.com/graph/edmonds_karp.html#implementation
 vector<vector<int>> capacity;
 vector<vector<int>> adj;
 
@@ -26,7 +26,7 @@ int bfs(int s, int t, vector<int>& parent) {
     fill(parent.begin(), parent.end(), -1);
     parent[s] = -2;
     queue<pair<int, int>> q;
-    q.push({s, INT_MAX});
+    q.emplace(s, INT_MAX);
 
     while (!q.empty()) {
         int cur = q.front().first;
@@ -39,11 +39,10 @@ int bfs(int s, int t, vector<int>& parent) {
                 int new_flow = min(flow, capacity[cur][next]);
                 if (next == t)
                     return new_flow;
-                q.push({next, new_flow});
+                q.emplace(next, new_flow);
             }
         }
     }
-
     return 0;
 }
 
@@ -67,45 +66,43 @@ int maxflow(int s, int t) {
 }
 int main(){
     int u, v;
-    int S = n-2;
-    int T = n-1;
+    int S = 0, T;
     vector<int> res;
 
     while (true){
         scanf("%d %d", &n, &m);
         if (n == 0 && m == 0){break;}
+        T = n+1;
+        adj.clear();
+        capacity.clear();
+        adj.resize(n+2);
+        capacity.resize(n+2, vector<int>(n+2, 0));
 
-        estudiantes.resize(n+1);
-        aristas.resize(n+1+2); //aristas[S] = s; aristas[S] = t;
-
-        capacity.resize(vector<int>(n+2, 0));
+        estudiantes.resize(n+2);
 
         for (int i = 1; i <= n; i++){
             scanf("%d", &estudiantes[i]);
             if (estudiantes[i] == 1){
-                aristas[S].emplace_back(i, 1);
-                aristas[i].emplace_back(S, 1);
                 adj[S].push_back(i);
+                capacity[S][i] = 1;
             }else{
-                aristas[i].emplace_back(T, 1);
-                aristas[T].emplace_back(i, 1);
                 adj[i].push_back(T);
+                capacity[i][T] = 1;
             }
         }
 
         for (int i = 0; i < m; i++){
             scanf("%d %d", &u, &v);
-            aristas[u].emplace_back(v, 1);
-            aristas[v].emplace_back(u, 1);
             adj[u].push_back(v);
             adj[v].push_back(u);
+            capacity[u][v] = 1;
+            capacity[v][u] = 1;
         }
-        n +=2;
         res.push_back(maxflow(S, T));
     }
 
-    for(int i = 0; i < res.size(); i++){
-        printf("%d\n", res[i]);
+    for(int & r : res){
+        printf("%d\n", r);
     }
     return 0;
 }
